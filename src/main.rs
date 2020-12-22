4a6c5151d0978437f32384dcb926308360f5d9d9
xianwill <christianwilliams79@gmail.com>
2020-12-22 07:13:41 -0500

    Re-structure code for better testability

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,13 @@
-#[macro_use]
-extern crate serde_derive;
-
-#[macro_use]
-extern crate nonzero_ext;
-
 use async_std::task;
-use futures::sink::SinkExt;
-use futures::StreamExt;
 use governor::{Quota, RateLimiter};
 use log::*;
-use nonzero_ext::nonzero;
 use rdkafka::config::ClientConfig;
 use rdkafka::producer::{DeliveryFuture, FutureProducer, FutureRecord};
 use std::fs::File;
-use std::io::prelude::*;
 use std::io::{BufRead, BufReader, Lines};
+use std::ops::FnMut;
 use std::path::{Path, PathBuf};
-use std::time::{Duration, Instant};
+use std::time::Instant;
 use structopt::StructOpt;
 
 fn main() {
@@ -24,92 +15,107 @@ fn main() {
 
     let opt = Opt::from_args();
 
-    println!("Starting player. Options {:?}", opt);
+    info!("Will play messages with options {:#?}", opt);
+
+    let kafka_config = ClientConfig::from(&opt);
+    let producer: FutureProducer = kafka_config.create().expect("Producer creation failed");
 
     let path: &Path = opt.message_file.as_path();
-
-    // load the message file
     let file = File::open(path).expect(format!("Failed to open file {:?}", path).as_str());
     let reader = BufReader::new(file);
 
-    let mut kafka_config: ClientConfig = ClientConfig::new();
+    // initialize a timer to track message rate
+    let start = Instant::now();
 
-    kafka_config.set("bootstrap.servers", &opt.bootstrap_servers);
-
-    println!("Starting produce loop.");
-
-    // block on `play` which awaits an async rate limiter.
-    task::block_on(play(
+    // block on `read_all` which awaits an async rate limiter.
+    task::block_on(read_all(
         reader.lines(),
-        opt.topic,
         opt.message_rate,
         opt.message_count,
-        kafka_config,
+        // send to kafka and check progress on each callback from the read loop 
+        |i, line| {
+            send_to_kafka(line, &opt.topic, &producer);
+            check_progress(i, opt.progress_interval, &start);
+        },
     ));
 }
 
-async fn play<T>(
-    lines: Lines<T>,
-    topic: String,
-    message_rate: u32,
-    message_count: u32,
-    kafka_config: ClientConfig,
-) where
+/// Reads all messages in the iterator and invokes the given callback on each one.
+async fn read_all<T, F>(lines: Lines<T>, message_rate: u32, message_count: u32, mut callback: F)
+where
     T: BufRead,
+    F: FnMut(u32, String),
 {
-    // setup rate limiter
-    let lim = RateLimiter::direct(Quota::per_second(
+    // setup rate limiter to send `message_rate` messages per second
+    let limiter = RateLimiter::direct(Quota::per_second(
         nonzero_ext::NonZero::new(message_rate).unwrap(),
     ));
+
+    // initialize send count
     let mut send_count = 0u32;
-    let start = Instant::now();
 
-    // setup the kafka producer
-    let producer: FutureProducer = kafka_config.create().expect("Producer creation failed");
-
+    // iterate lines and play each one
     for result in lines {
-        debug!("Handling line");
         if send_count == message_count {
-            println!("Sent {} messages. Terminating.", send_count);
+            info!("Sent {} lines. Terminating.", send_count);
             break;
-        }
-        // TODO: Add progress interval to config
-        if send_count % 1000 == 0 && send_count > 0 {
-            let elapsed_millis = start.elapsed().as_millis() as f64;
-            let elapsed_seconds = start.elapsed().as_secs_f32();
-            let per_milli = send_count as f64 / elapsed_millis;
-            let per_sec = per_milli * 1000f64;
-            println!(
-                "Message rate is {} per millisecond ({} per second). Sent {} messages in {} seconds ({} minutes).",
-                per_milli,
-                per_sec,
-                send_count,
-                elapsed_seconds,
-                elapsed_seconds / 60 as f32
-            );
         }
         match result {
             Ok(line) => {
-                let record: FutureRecord<String, String> = FutureRecord::to(&topic).payload(&line);
-                debug!("Spawning producer send");
-                spawn_and_log_error(producer.send(record, -1i64));
+                callback(send_count, line);
             }
             Err(e) => {
-                eprintln!("Failed to read line {:?}", e);
+                error!("Failed to read line {:?}", e);
             }
         }
         send_count += 1;
-        debug!("Awaiting limiter");
-        lim.until_ready().await
+        limiter.until_ready().await
     }
 }
 
+/// Writes the line to the specified Kafka topic.
+fn send_to_kafka(line: String, topic: &str, producer: &FutureProducer) {
+    let record: FutureRecord<String, String> = FutureRecord::to(topic).payload(&line);
+    spawn_and_log_error(producer.send(record, -1i64));
+}
+
+/// Logs current progress.
+fn check_progress(send_count: u32, progress_interval: u32, start: &Instant) {
+    if send_count % progress_interval == 0 && send_count > 0 {
+        let elapsed_millis = start.elapsed().as_millis() as f64;
+        let elapsed_seconds = start.elapsed().as_secs_f32();
+        let per_milli = send_count as f64 / elapsed_millis;
+        let per_sec = per_milli * 1000f64;
+        info!(
+            "Message rate is {} per millisecond ({} per second). Sent {} messages in {} seconds ({} minutes).",
+            per_milli,
+            per_sec,
+            send_count,
+            elapsed_seconds,
+            elapsed_seconds / 60 as f32
+        );
+    }
+}
+
+/// Spawns a separate task on which to await the future, logging an error result if appropriate.
 fn spawn_and_log_error(fut: DeliveryFuture) -> task::JoinHandle<()> {
     task::spawn(async move {
         if let Err(e) = fut.await {
-            eprintln!("{}", e)
+            error!("{:?}", e)
         }
     })
+}
+
+/// Creates an `rdkafka::config::ClientConfig` from an `Opt`.
+impl From<&Opt> for ClientConfig {
+    fn from(opt: &Opt) -> Self {
+        let mut kafka_config: ClientConfig = ClientConfig::new();
+
+        // TODO: accept kafka props as a list and set them all
+        kafka_config.set("bootstrap.servers", &opt.bootstrap_servers);
+
+        kafka_config
+    }
 }
 
 #[derive(Debug, StructOpt)]
@@ -143,4 +149,27 @@ struct Opt {
     /// The number of messages to play per second.
     #[structopt(short = "r", long, default_value = "1")]
     message_rate: u32,
+
+    /// The number of messages to wait for between progress reports.
+    #[structopt(short = "p", long, default_value = "1000")]
+    progress_interval: u32,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn config_from_opt_test() {
+
+    }
+
+    #[test]
+    fn read_all_test() {}
+
+    #[test]
+    fn spawn_and_log_error_test() {}
+
+    #[test]
+    fn check_progress_test() {}
 }
